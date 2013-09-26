920c9a3e20721358f23f8651a8c3a0c9d7bd4e49
narendrabtechcse <narendrabtechcse@gmail.com>
2013-09-26 10:02:09 +0530

    Create TestTaskA.java
    TestTaskA.java of algo2 question1

---
+++ TestTaskA.java
@@ -0,0 +1,24 @@
+package Coursera;
+
+
+	import java.io.*;
+
+	public class TestTaskA {
+
+	public static void main(String[] s) throws IOException {
+	InputStream inputStream = TestTaskA.class.getResourceAsStream("jobs.txt");
+	InputReader in = new InputReader(inputStream);
+
+	PrintWriter out = new PrintWriter(System.out);
+
+	long startTime = System.nanoTime();
+
+	new TaskA().solve(in, out);
+
+	long endTime = System.nanoTime();
+	out.println("elapsed time " + (endTime - startTime) / 1000000 + "milli secs");
+
+	inputStream.close();
+	out.flush();
+	}
+	}
